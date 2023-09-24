2f3570339d18ccd0f24d965f073c039eb5fe4d21
LukazTorres <ltorreschileno@gmail.com>
2023-09-24 15:25:52 -0500

    crear Backend

---
+++ BackendMolino/src/main/java/com/PortalWebMolino/BackendMolino/BackendMolinoApplication.java
@@ -0,0 +1,13 @@
+package com.PortalWebMolino.BackendMolino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMolinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMolinoApplication.class, args);
+	}
+
+}
---
+++ BackendMolino/src/test/java/com/PortalWebMolino/BackendMolino/BackendMolinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortalWebMolino.BackendMolino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMolinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
