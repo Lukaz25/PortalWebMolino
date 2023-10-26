397e0cf0a7ccb10700f3afd8bc96e219c7441748
LukazTorres <ltorreschileno@gmail.com>
2023-10-26 02:46:53 -0500

    update frontend

--- FrontendMolino/src/app/Components/layout/layout.component.ts
+++ FrontendMolino/src/app/Components/layout/layout.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {
-
+export class LayoutComponent implements OnInit {
+  showMenu1 = false;
+  showMenu2 = false;
+  showMenu3 = false;
+  showMenu4 = false;
+  constructor() {}
+  ngOnInit(): void {
+    this.toggleMenu2();
+  }
+toggleMenu1() {
+this.showMenu1 = !this.showMenu1;
+ }
+ toggleMenu2() {
+  this.showMenu2 = !this.showMenu2;
 }
+toggleMenu3() {
+  this.showMenu3 = !this.showMenu3;
+}
+toggleMenu4() {
+  this.showMenu4 = !this.showMenu4;
+}
+}
+
